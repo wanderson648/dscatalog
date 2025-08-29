ed1dc444147f070d6cf330ce73a2638f561ef1cb
wanderson648 <testepedroca@gmail.com>
2025-08-28 21:45:48 -0300

    initial projet

---
+++ src/main/java/com/devsuperior/auladev/AuladevApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.auladev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuladevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuladevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/auladev/AuladevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.auladev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuladevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
